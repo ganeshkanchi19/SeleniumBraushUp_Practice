01305f4c6e7d2a2654574bcec70dd709552564a6
ganeshkanchi19 <ganukanchi2018@gmail.com>
2025-11-25 20:34:19 +0530

    Added DriverFactory with ThreadLocal WebDriver, Chrome initialization, and headless CI support.

---
+++ src/main/java/utils/DriverFactory.java
@@ -0,0 +1,58 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	private static final ThreadLocal<WebDriver> TL_DRIVER = new ThreadLocal<>();
+
+	public static WebDriver getDriver() {
+		return TL_DRIVER.get();
+	}
+
+	public static void setDriver(WebDriver driver) {
+		TL_DRIVER.set(driver);
+	}
+
+	public static void removeDriver() {
+		WebDriver drv = TL_DRIVER.get();
+		if (drv != null) {
+			try {
+				drv.quit();
+			} catch (Exception e) {
+				/* ignore */ }
+			TL_DRIVER.remove();
+		}
+	}
+
+	/**
+	 * Initialize a ChromeDriver for this thread. You can switch to other browsers
+	 * by adding similar methods.
+	 */
+	public static void initChromeDriver() {
+		// Setup chromedriver binary 
+		WebDriverManager.chromedriver().setup();
+
+		ChromeOptions options = new ChromeOptions();
+
+		// If you want headless on CI, set system property -Dheadless=true
+		String headless = System.getProperty("headless", "false");
+		if (headless.equalsIgnoreCase("true")) {
+			// newer chrome headless flag
+			options.addArguments("--headless=new");
+			options.addArguments("--disable-gpu");
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+		}
+
+		// Common options
+		options.addArguments("--window-size=1920,1080");
+
+		WebDriver driver = new ChromeDriver(options);
+		setDriver(driver);
+	}
+
+}
