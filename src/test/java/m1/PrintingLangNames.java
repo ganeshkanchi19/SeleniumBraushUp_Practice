23299a6c2855ec95cbb33a68edaa44dc3b34a10a
ganeshkanchi19 <ganukanchi2018@gmail.com>
2026-01-23 15:57:22 +0530

    Printing the Languages as per our need

---
+++ src/test/java/m1/PrintingLangNames.java
@@ -0,0 +1,23 @@
+package m1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.CurrencyTablePage;
+
+public class PrintingLangNames extends BaseTest {
+
+	@Test
+	public void printEuroNames() {
+
+		CurrencyTablePage currencyPage = new CurrencyTablePage(getDriver());
+
+		currencyPage.openPage();
+		currencyPage.printGermanLangNames();
+
+		System.out.println("Count of German words is : " + currencyPage.getGermanLangCount());
+		Assert.assertEquals(currencyPage.getGermanLangCount(), 3);
+	}
+
+}
