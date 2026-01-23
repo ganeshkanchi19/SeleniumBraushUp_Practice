aec07763b5d1a13da3b5bb17327110c158714e1d
ganeshkanchi19 <ganukanchi2018@gmail.com>
2026-01-23 17:08:12 +0530

    Master conflict change

--- src/test/java/m1/PrintingLangNames.java
+++ src/test/java/m1/PrintingLangNames.java
@@ -17,7 +17,7 @@ public class PrintingLangNames extends BaseTest {
 		currencyPage.printGermanLangNames();
 
 		System.out.println("Count of German words is : " + currencyPage.getGermanLangCount());
-		System.out.println("Master branch change");
+		System.out.println("MASTER_CONFLICT");
 		Assert.assertEquals(currencyPage.getGermanLangCount(), 3);
 		System.out.println("Master branch conflcit coming");
 	}
